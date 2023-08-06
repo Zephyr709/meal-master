c2cced995b94d46e135e9525b98c5c0944d309a2
Jager Cooper <100608609+Zephyr709@users.noreply.github.com>
2023-08-06 16:15:54 -0230

    Days of the week tab/responsive implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import ResponsiveAppBar from './components/AppBar';
 import LabelBottomNavigation from './components/LabelBottomNavigation';
 import Box from '@mui/material/Box';
+import DayTabs from './components/dayTabs';
 
 function App() {
   return (
     <Box>
       <ResponsiveAppBar />
-      
+      <DayTabs />  
       <LabelBottomNavigation />
     </Box>
 
---
+++ src/components/dayTabs.tsx
@@ -0,0 +1,80 @@
+import { Box, Tab, Tabs } from "@mui/material";
+import * as React from 'react';
+
+
+export default function DayTabs() {
+    const [value, setValue] = React.useState(0);
+    // State to track the current viewport width
+    const [viewportWidth, setViewportWidth] = React.useState(window.innerWidth);
+
+    // Function to handle viewport width changes
+    const handleViewportWidthChange = () => {
+        setViewportWidth(window.innerWidth);
+    };
+
+    // Effect hook to subscribe to window resize events
+    React.useEffect(() => {
+        window.addEventListener('resize', handleViewportWidthChange);
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+        window.removeEventListener('resize', handleViewportWidthChange);
+
+    }});
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue);
+    };
+
+
+    const days = [
+        {id: 0, shortForm: 'Mon', longForm: 'Monday'},
+        {id: 1, shortForm: 'Teus', longForm: 'Teusday'},
+        {id: 2, shortForm: 'Wed', longForm: 'Wednesday'},
+        {id: 3, shortForm: 'Thur', longForm: 'Thursday'},
+        {id: 4, shortForm: 'Fri', longForm: 'Friday'},
+        {id: 5, shortForm: 'Sat', longForm: 'Saturday'},
+        {id: 6, shortForm: 'Sun', longForm: 'Sunday'},
+    ];
+
+    // Renders in shorthand form and with scroll buttons if Component exceed viewport width
+    if (viewportWidth < 700) {
+           return (
+        <Box >
+            <Tabs 
+            value={value} 
+            onChange={handleChange}
+            variant='scrollable'
+            scrollButtons
+            allowScrollButtonsMobile
+            >
+                {days.map(day => (
+                    <Tab 
+                    label={day.shortForm}
+                    />
+                ))}
+               
+            </Tabs>
+        </Box>  
+    ) 
+    } else {
+        return (
+            <Box >
+            <Tabs 
+            value={value} 
+            onChange={handleChange}
+            centered
+            allowScrollButtonsMobile
+            >
+                {days.map(day => (
+                    <Tab 
+                    label={day.longForm}
+                    />
+                ))}
+            
+            </Tabs>
+        </Box>  
+        )    
+    }
+
+}
