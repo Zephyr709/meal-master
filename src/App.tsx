3788942ddf30e5f3c715d34eb89bdd79f2114de0
Jager Cooper <100608609+Zephyr709@users.noreply.github.com>
2023-08-20 09:46:34 -0230

    implemented theme provider and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 import './App.css';
 import ResponsiveAppBar from './components/AppBar';
 import LabelBottomNavigation from './components/LabelBottomNavigation';
 import Box from '@mui/material/Box';
 import DayTabs from './components/dayTabs';
 import PlannedRecipes from './components/PlannedRecipes';
+import { CssBaseline,  ThemeProvider, createTheme } from '@mui/material';
 
 
-function App() {
+
+const theme = createTheme();
+
+const App: React.FC = () => {
   return (
-    <Box>
-      <ResponsiveAppBar />
-      <DayTabs/>
-      <PlannedRecipes/>
-      <LabelBottomNavigation />
-    </Box>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Router>
+        <ResponsiveAppBar />
+        <Box>
+          
+          <DayTabs/>
+          <PlannedRecipes/>
+          <LabelBottomNavigation />
+        </Box>
+        
+        
+        <Routes>
+          <Route path="/"  />
+          <Route path="/recipes" />
+          <Route path="/pantry" />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+   
 
 
   )
